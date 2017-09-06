45a49b6c05a03592b000b88df2106066abdb3ff0
Anapher (Vincent) <anapher@protonmail.com>
2017-09-06 22:28:37 +0200

    that's how it begins...

---
+++ src/ReferencedAssembly/ConfigureAwaitAttribute.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Fody
+{
+    /// <summary>
+    ///     Use this attribute to configure all unconfigured awaits in the region
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Assembly | AttributeTargets.Class | AttributeTargets.Struct |
+                    AttributeTargets.Method)]
+    public class ConfigureAwaitAttribute : Attribute
+    {
+        /// <summary>
+        ///     Configures all awaiters used in the region this attribute was applied on
+        /// </summary>
+        /// <param name="continueOnCapturedContext">
+        ///     <code>true</code> to attempt to marshal the continuation back to the original
+        ///     context captured; otherwise, <code>false</code>.
+        /// </param>
+        public ConfigureAwaitAttribute(bool continueOnCapturedContext)
+        {
+        }
+    }
+}
